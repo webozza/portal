72b62fa70d8c8aed3e75a5d07b8056b98c8ad417
webozza <webozza@gmail.com>
2023-05-15 18:14:12 +0800

    version update

--- template-parts/modals/forgot-password.php
+++ template-parts/modals/forgot-password.php
@@ -9,7 +9,7 @@
             <a class="close-modal" href="javascript:void(0)"><img src="<?= get_template_directory_uri() . '/img/icons/close.png' ?>"></a>
         </div>
         <div class="cure-modal-body">
-            <iframe src="<?= $root . '/wp-login.php?action=lostpassword' ?>"></iframe>
+            <?= do_shortcode('[custom-password-lost-form]') ?>
         </div>
     </div>
 </div>
@@ -24,3 +24,4 @@
         })
     });
 </script>
+
