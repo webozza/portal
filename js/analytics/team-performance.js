ae6303f6f8f1d6e446cd333124844d4c1d0248fb
webozza <webozza@gmail.com>
2023-06-23 08:23:54 +0800

    billable target completed

--- js/analytics/team-performance.js
+++ js/analytics/team-performance.js
@@ -118,7 +118,6 @@ let compareHoursPH = async (start_date, end_date, time_frame, time_status) => {
     $(".status-text > img").hide();
     let bgColor;
 
-    // Check if the user ID matches the specific ID requiring the billable target
     // Check if the user ID matches the specific IDs requiring different billable targets
     if (
       (thisUserID_PH === 5752190760 ||
@@ -126,28 +125,26 @@ let compareHoursPH = async (start_date, end_date, time_frame, time_status) => {
         thisUserID_PH === 12344835539) &&
       time_status === "billable"
     ) {
-      let billableTarget;
+      billableTarget = 20; // Set the billable target percentage for specific user IDs
+    } else if (
+      (thisUserID_PH === 5819640839 || thisUserID_PH === 6146212654) &&
+      time_status === "billable"
+    ) {
+      billableTarget = 60; // Set the billable target percentage for specific user IDs
+    } else {
+      billableTarget = 87.5; // Set the billable target percentage for the rest
+    }
 
-      if (thisUserID_PH === 5819640839 || thisUserID_PH === 6146212654) {
-        billableTarget = 60; // Set the billable target percentage for specific user IDs
-      } else if (
-        thisUserID_PH === 8024149849 ||
-        thisUserID_PH === 12344835539
-      ) {
-        billableTarget = 20; // Set the billable target percentage for specific user IDs
-      } else {
-        billableTarget = 87.5; // Set the billable target percentage for the rest
-      }
-
+    if (billableTarget !== undefined) {
       if (thisUserHits < billableTarget) {
         thisUser.find(".user-status > div").hide();
         thisUser.find(".user-status .under").show();
         bgColor = "#FF605C";
-      } else if (thisUserHits >= billableTarget && thisUserHits <= 100) {
+      } else if (thisUserHits >= billableTarget && thisUserHits <= 120) {
         thisUser.find(".user-status > div").hide();
         thisUser.find(".user-status .on-target").show();
         bgColor = "#00CA4E";
-      } else if (thisUserHits > 100) {
+      } else if (thisUserHits > 120) {
         thisUser.find(".user-status > div").hide();
         thisUser.find(".user-status .over").show();
         bgColor = "#808080";
